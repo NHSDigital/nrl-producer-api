d66bb1b33feea97afd2b1960b11ba57442ab5faa
Chris Kemp <chris.kemp3@nhs.net>
2022-11-30 15:28:03 +0000

    Removed destructuring because of potential JS version in APIGEE

--- proxies/live/apiproxy/resources/jsc/RewritePath.js
+++ proxies/live/apiproxy/resources/jsc/RewritePath.js
@@ -1,5 +1,6 @@
-const { rewrite } = require('./utils');
-const { input, output } = properties;
+const rewrite = require('./utils');
+const input = properties.input;
+const output = properties.output;
 
 const locationHeader = context.getVariable("message.header.Location");
 const newHeader = rewrite(locationHeader, input, output);
