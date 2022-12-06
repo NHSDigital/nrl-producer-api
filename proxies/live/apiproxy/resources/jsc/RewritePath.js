2bdd1c7acf103e23ee04eade007e1d3c3cbbe83e
Chris Kemp <chris.kemp3@nhs.net>
2022-12-06 13:33:37 +0000

    APIGEE JS is _terrible_

--- proxies/live/apiproxy/resources/jsc/RewritePath.js
+++ proxies/live/apiproxy/resources/jsc/RewritePath.js
@@ -1,6 +1,9 @@
-const rewrite = require('./utils');
 const input = properties.input;
 const output = properties.output;
+
+function rewriteIfMatch(path, input, output) {
+    return path.replace(RegExp("^" + input + "$"), output);
+}
 
 const locationHeader = context.getVariable("message.header.Location");
 const newHeader = rewrite(locationHeader, input, output);
