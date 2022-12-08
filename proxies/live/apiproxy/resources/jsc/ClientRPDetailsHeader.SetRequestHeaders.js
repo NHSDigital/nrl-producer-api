7c326320e2a44d7823615ac7256e9358a037d259
Chris Kemp <chris.kemp3@nhs.net>
2022-12-08 10:22:28 +0000

    Added Comments

--- proxies/live/apiproxy/resources/jsc/ClientRPDetailsHeader.SetRequestHeaders.js
+++ proxies/live/apiproxy/resources/jsc/ClientRPDetailsHeader.SetRequestHeaders.js
@@ -1,3 +1,16 @@
+/*
+ * This script is used to set the NHSD-Client-RP-Details header, which is passed
+ * to the API.
+ * 
+ * this contains:
+ * 
+ *  developer.app.id                                - The APIGEE ID associated with the APIGEE App connecting
+ *  developer.app.name                              - The name of the APIGEE App
+ *  developer.app.nhs-login-minimum-proofing-level  - The P0/P5/P9 requirement when using User Based Authentication
+ *  client.ip                                       - The client's IP address
+ * 
+ */
+
 var developerAppName = context.getVariable('developer.app.name');
 var developerAppId = context.getVariable('developer.app.id');
 var clientIP = context.getVariable('client.ip');
