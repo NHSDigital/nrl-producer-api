2e6af3c347e7d08cc90e9e281b6ea019ebbea1bd
Brian Bayes <brian.bayes@live.co.uk>
2023-02-09 13:31:43 +0000

    [NRLF-273] Environment check in Makefile

--- tests/configuration/config.py
+++ tests/configuration/config.py
@@ -1,13 +1,6 @@
 from .environment import ENV
 
-apigee_app_ids = {
-    "int": "7a4ad8c7-3927-4259-af7b-ac9d305a86ad",
-    "prod": "881f4128-2dcd-4a61-90a6-33af19c3c202",
-    "internal-dev": "123456"
-}
-
 # Api Details
 ENVIRONMENT = ENV["environment"]
 BASE_URL = f"https://{ENVIRONMENT}.api.service.nhs.uk"
 BASE_PATH = ENV["base_path"]
-APIGEE_APP_ID = apigee_app_ids[ENVIRONMENT]
