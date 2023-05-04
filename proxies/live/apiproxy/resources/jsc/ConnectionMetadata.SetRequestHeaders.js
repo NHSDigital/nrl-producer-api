da5433d52022a1565f06d687727a715db25d3741
Brian Bayes <brian.bayes@live.co.uk>
2023-05-04 09:33:43 +0100

    [NRLF-395] Address null check on nrlPermissions PR comment

--- proxies/live/apiproxy/resources/jsc/ConnectionMetadata.SetRequestHeaders.js
+++ proxies/live/apiproxy/resources/jsc/ConnectionMetadata.SetRequestHeaders.js
@@ -1,16 +1,16 @@
 /*
  * This script reads a Custom Attribute from the APIGEE app and sends it to the API
  * as the `NHSD-Connection-Metadata` header.
- * 
+ *
  * The Http Header 'NHSD-End-User-Organisation-ODS' is expected.
  * The Custom Attribute `nrl-ods-<ods_code>` is transformed from:
- * 
+ *
  * e.g.
- * 
+ *
  * HTTP Request Header          = ods_code: RJ11
  * APIGEE App Custom Attribute  = nrl-ods-RJ11: http://snomed.info/sct|736253001\nhttp://snomed.info/sct|736253002\n
- * 
- * Http Response Header = NHSD-Connection-Metadata: 
+ *
+ * Http Response Header = NHSD-Connection-Metadata:
  *  {
  *      "nrl.ods-code": "RJ11",
  *      "nrl.pointer-types": [
@@ -20,53 +20,61 @@
  *  }
  */
 
-(function(){
-    // Read the 'NHSD-End-User-Organisation-ODS' header
-    var odsCode = context.getVariable('request.header.NHSD-End-User-Organisation-ODS');
-    if (!odsCode || odsCode.trim().length === 0) {
-        //This will trigger RaiseFault.400BadRequest.xml - see proxies/deafult.xml in the DefaultFaultRules
-        return;
+(function () {
+  // Read the 'NHSD-End-User-Organisation-ODS' header
+  var odsCode = context.getVariable(
+    "request.header.NHSD-End-User-Organisation-ODS"
+  );
+  if (!odsCode || odsCode.trim().length === 0) {
+    //This will trigger RaiseFault.400BadRequest.xml - see proxies/deafult.xml in the DefaultFaultRules
+    return;
+  }
+
+  // Read the associated `nrl-ods-<ods_code>` custom attribute from the APIGEE app
+  var nrlPointerTypes = context.getVariable("app.nrl-ods-" + odsCode);
+  if (!nrlPointerTypes) {
+    //This will trigger RaiseFault.403NoPointers.xml - see targets/target.xml
+    return;
+  }
+
+  // Convert it into a complex object
+  var lines = nrlPointerTypes.split(/\s+/);
+  var pointerTypes = [];
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (line && line.trim().length !== 0) {
+      pointerTypes.push(line);
     }
+  }
 
-    // Read the associated `nrl-ods-<ods_code>` custom attribute from the APIGEE app
-    var nrlPointerTypes = context.getVariable('app.nrl-ods-' + odsCode);
-    if (!nrlPointerTypes) {
-        //This will trigger RaiseFault.403NoPointers.xml - see targets/target.xml
-        return;
-    }
+  var odsCodeExtension = context.getVariable(
+    "request.header.NHSD-End-User-Organisation"
+  );
 
-    // Convert it into a complex object
-    var lines = nrlPointerTypes.split(/\s+/);
-    var pointerTypes = [];
-    for (var i=0;i<lines.length;i++) {
-        var line = lines[i];
-        if (line && line.trim().length !== 0) {
-            pointerTypes.push(line);
-        }
-    }
+  // Build the response
+  var connectionMetadata = {
+    "nrl.ods-code": odsCode,
+    "nrl.ods-code-extension": odsCodeExtension,
+    "nrl.pointer-types": pointerTypes,
+    "nrl.permissions": permissions,
+  };
 
-    var odsCodeExtension = context.getVariable(
-        "request.header.NHSD-End-User-Organisation"
-    );
+  var nrlPermissions = context.getVariable("app.nrl-permissions");
 
-    var nrlPermissions = context.getVariable('app.nrl-permissions');
-
+  if (nrlPermissions != null) {
     // Convert it into a complex object
     var permissionLines = nrlPermissions.split(/\s+/);
     var permissions = [];
-    for (var i=0;i<permissionLines.length;i++) {
-        var permissionLine = permissionLines[i];
-        if (permissionLine && permissionLine.trim().length !== 0) {
-            permissions.push(permissionLine);
-        }
+    for (var i = 0; i < permissionLines.length; i++) {
+      var permissionLine = permissionLines[i];
+      if (permissionLine && permissionLine.trim().length !== 0) {
+        permissions.push(permissionLine);
+      }
     }
 
-    // Build the response
-    var connectionMetadata = {
-        "nrl.ods-code": odsCode,
-        "nrl.ods-code-extension": odsCodeExtension,
-        "nrl.pointer-types": pointerTypes,
-        "nrl.permissions": permissions,
-    };
-    context.targetRequest.headers['NHSD-Connection-Metadata'] = connectionMetadata;
+    connectionMetadata["nrl.permissions"] = permissions;
+  }
+
+  context.targetRequest.headers["NHSD-Connection-Metadata"] =
+    connectionMetadata;
 })();
--- proxies/sandbox/apiproxy/resources/jsc/ConnectionMetadata.SetRequestHeaders.js
+++ proxies/sandbox/apiproxy/resources/jsc/ConnectionMetadata.SetRequestHeaders.js
@@ -9,20 +9,27 @@
 //---- CHANGE AVAILABLE POINTER TYPES FOR EACH ORGANISATION HERE ----//
 const nrlPointers = {
   // This one is needed for Smoke Tests
-  RJ11: [
-    "http://snomed.info/sct|736253001",
-    "http://snomed.info/sct|736253002",
+  "RJ11": [
+        "http://snomed.info/sct|736253001",
+        "http://snomed.info/sct|736253002"
   ],
   // These ones are needed for the Seed data
-  Y05868: [
+  "Y05868": [
     "http://snomed.info/sct|736253002",
     "http://snomed.info/sct|887701000000100",
     "http://snomed.info/sct|1363501000000100",
-    "http://snomed.info/sct|861421000000109",
+    "http://snomed.info/sct|861421000000109"
   ],
-  "8J008": ["http://snomed.info/sct|1363501000000100"],
-  RY26A: ["http://snomed.info/sct|861421000000109"],
-  RM559: ["http://snomed.info/sct|736253002"],
+  "8J008": [
+    "http://snomed.info/sct|1363501000000100"
+  ],
+  "RY26A": [
+    "http://snomed.info/sct|861421000000109"
+  ],
+  "RM559":[
+    "http://snomed.info/sct|736253002"
+  ]
+
 };
 //-------------------------------------------------------------------//
 
