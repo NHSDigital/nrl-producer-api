7fcd867e986572e51d6abf2efe39ceeee1f2607f
Kate Bobyn <kate.bobyn2@nhs.net>
2026-03-12 22:11:19 +0000

    NRL-2005 add sandbox app id header

--- proxies/live/apiproxy/resources/jsc/NrlAppId.SetRequestHeaders.js
+++ proxies/live/apiproxy/resources/jsc/NrlAppId.SetRequestHeaders.js
@@ -1,5 +1,5 @@
 (function () {
-  var nrlAppID = context.getVariable("app.nrl-app-id");
+  const nrlAppID = context.getVariable("app.nrl-app-id");
   if (!nrlAppID || nrlAppID.trim().length === 0) {
     //This will trigger RaiseFault.403MissingNRLAppID.xml - see targets/targets.xml
     return;
