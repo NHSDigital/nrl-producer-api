cbabf757d077c591b6c4efc89108379e272a950e
Kate Bobyn <kate.bobyn2@nhs.net>
2026-03-12 16:32:44 +0000

    NRL-2005 add NRL app ID in a separate header for all requests

---
+++ proxies/live/apiproxy/resources/jsc/NrlAppId.SetRequestHeaders.js
@@ -0,0 +1,9 @@
+(function () {
+  var nrlAppID = context.getVariable("app.nrl-app-id");
+  if (!nrlAppID || nrlAppID.trim().length === 0) {
+    //This will trigger RaiseFault.403MissingNRLAppID.xml - see targets/targets.xml
+    return;
+  }
+
+  context.targetRequest.headers["NHSD-NRL-App-ID"] = nrlAppID;
+})();
